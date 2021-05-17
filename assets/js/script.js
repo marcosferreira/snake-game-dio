39e39ac3fb7729b59768535e62c162e3d0c520d9
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-05-17 05:23:28 -0300

    create function for initialize game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,19 @@ let canvas = document.getElementById('snake');
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let direction = 'right';
 
 snake[0] = {
   x: 8 * box,
   y: 8 * box,
 }
 
-function createBackground() {
+const createBackground = () => {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function createSnake() {
+const createSnake = () => {
   for (let i = 0; i < snake.length; i++) {
     context.fillStyle = 'green';
     context.fillRect(snake[i].x, snake[i].y, box, box);
@@ -21,5 +22,26 @@ function createSnake() {
   }
 }
 
-createBackground();
-createSnake();
+const createGame = () => {
+  createBackground();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if(direction == 'right') snakeX += box;
+  if(direction == 'left') snakeX -= box;
+  if(direction == 'up') snakeY -= box;
+  if(direction == 'down') snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let gameInit = setInterval(createGame, 100);
